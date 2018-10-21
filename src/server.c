9540f037148ba1082ae3bb74ab4d3b24172dd84f
Sriram <sriram13k@gmail.com>
2018-10-21 12:03:07 +0530

    Updated server.c

--- src/server.c
+++ src/server.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<errno.h>
+#include<unistd.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include <netdb.h>
 
-#define BACKLOG 10
+#define BACKLOG 5
+#define MSG_SIZE 256
 
 void error(const char* error_msg)
 {
@@ -15,8 +17,59 @@ void error(const char* error_msg)
 }
 
 
-
 int main(int argc, char* argv[])
 {
-    
+    int sockfd, new_fd , portno;
+    socklen_t client_length;
+    char buffer[MSG_SIZE];
+    struct sockaddr_in serv_addr,client_addr;
+
+    if( argc < 2 ) {
+        fprintf(stderr, " usage %s portno\n", argv[0]);
+        exit(0);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if( sockfd < 0 ) {
+        error("ERROR opening socket");
+    }
+
+    memset(&serv_addr , 0 , sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if(bind(sockfd, (struct sockaddr* ) &serv_addr, sizeof(serv_addr)) < 0) {
+        error("ERROR on binding");
+    }
+
+    listen(sockfd,BACKLOG);
+    client_length = sizeof(sockaddr);
+    new_fd = accept(sockfd, (struct sockaddr *) &client_addr, &client_length);
+
+    if (new_fd < 0) {
+        error("ERROR on accept");
+    }
+
+    while (true) {
+
+        memset(buffer, 0 , MSG_SIZE);
+        if( read(new_fd, buffer , MSG_SIZE) < 0) {
+            error("ERROR reading from the socket");
+        }
+        printf("Client : %s", buffer);
+        memset(buffer,0,MSG_SIZE);
+        fgets(buffer,MSG_SIZE,stdin);
+        if( write(new_fd,buffer,strlen(buffer)) < 0) {
+            error("ERROR writing to the socket");
+        }
+
+    }
+
+    close(new_fd);
+    close(sockfd);
+    return 0;
+
 }
